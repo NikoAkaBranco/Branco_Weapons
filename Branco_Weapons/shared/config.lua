a525b0335df73805ff3b28feb714c38e55cfaafd
NikoAkaBranco <157228083+NikoAkaBranco@users.noreply.github.com>
2024-01-20 16:27:15 +0100

    Add files via upload

---
+++ Branco_Weapons/shared/config.lua
@@ -0,0 +1,14 @@
+-- Script by Branco ##Branco_Weapons
+Config = {}
+-- Make sure to change the modifier ## damage (SetWeaponDamageModifier(`WEAPON_CARBINERIFLE`, 0.8))
+Config.Weapons = {
+    [`WEAPON_UNARMED`] = {model = `WEAPON_UNARMED`, modifier = 0.5, disableCritical = true},
+    [`WEAPON_NIGHTSTICK`] = {model = `WEAPON_NIGHTSTICK`, modifier = 0.4, disableCritical = true},
+    [`WEAPON_CARBINERIFLE`] = {model = `weapon_carbinerifle`, modifier = 0.8, disableCritical = true},
+    [`WEAPON_PISTOL50`] = {model = `weapon_pistol50`, modifier = 0.9, disableCritical = true},
+    [`WEAPON_SMG`] = {model = `weapon_smg`, modifier = 1.5, disableCritical = true},
+    [`WEAPON_MINISMG`] = {model = `weapon_minismg`, modifier = 1.5, disableCritical = true},
+    [`WEAPON_PISTOL`] = {model = `weapon_pistol`, modifier = 1.5, disableCritical = true},
+    [`WEAPON_COMBATPISTOL`] = {model = `weapon_combatpistol`, modifier = 1.5, disableCritical = true},
+    [`WEAPON_SNSPISTOL`] = {model = `weapon_snspistol`, modifier = 1.5, disableCritical = true},
+}

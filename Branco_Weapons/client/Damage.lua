aa8b81fe5cc310510df78d467c627e852037ac2d
NikoAkaBranco <157228083+NikoAkaBranco@users.noreply.github.com>
2024-05-28 23:02:51 +0200

    Update Damage.lua

--- Branco_Weapons/client/Damage.lua
+++ Branco_Weapons/client/Damage.lua
@@ -2,13 +2,13 @@
 CreateThread(function()
     while true do
         Wait(0)
-     
+      
         local playerPed = GetPlayerPed(-1)
         local weaponsConfig = Config.Weapons[GetSelectedPedWeapon(playerPed)]
         
         if weaponsConfig then
             if weaponsConfig.disableCritical then
-                SetPedSuffersCriticalHits(playerPed, false)
+                SetPedSuffersCriticalHits(playerPed, true)
             end
             SetWeaponDamageModifier(weaponsConfig.model, weaponsConfig.modifier)
         else
